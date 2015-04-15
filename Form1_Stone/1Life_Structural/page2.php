76550452b6ebd61539727266bdb7cc0905a4ddfb
Smriti.Singh <smritis.31095@gmail.com>
2015-04-16 02:58:25 +0530

    Minor changes to some pages.

--- Form1_Stone/1Life_Structural/page2.php
+++ Form1_Stone/1Life_Structural/page2.php
@@ -20,27 +20,5 @@
     <input type="checkbox" name="_3d" value='N'/>No
     <br/><br/>
 
-    <p>4. Structural Aspects</p>
-
-    <p>a. The roof is constructed such that it is not integral within itself (i.e., it does not act as a single unit and breaks open during earthquake shaking) and is not anchored into the walls</p>
-    <input type="checkbox" name="_4a" value='-2'/>Yes
-    <input type="checkbox" name="_4a" value='N'/>No
-
-    <p>b. The walls are thick and made in two wythes</p>
-    <input type="checkbox" name="_4b" value='Y'/>Yes
-    <input type="checkbox" name="_4b" value='N'/>No
-
-    <p>c. The walls are not integrated into each other at the corners</p>
-    <input type="checkbox" name="_4c" value='Y'/>Yes
-    <input type="checkbox" name="_4c" value='N'/>No
-    <br/><br/>
-
-    <p>5. Construction Details</p>
-
-    <p>a. The walls of the house are made with mud mortar and are exposed to the vagaries of the outside weather (especially rain water beating)</p>
-    <input type="checkbox" name="_5a" value='Y'/>Yes
-    <input type="checkbox" name="_5a" value='N'/>No
-    <br/><br/>
-
     <input type="submit"/>Submit
 </form>
