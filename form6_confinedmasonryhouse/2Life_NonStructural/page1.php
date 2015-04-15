a060af60dfc2b438dbb6906726bcb9d6d92d41b3
Smriti.Singh <smritis.31095@gmail.com>
2015-04-16 03:05:50 +0530

    Division of content into more pages for forms 6 and 7.

--- form6_confinedmasonryhouse/2Life_NonStructural/page1.php
+++ form6_confinedmasonryhouse/2Life_NonStructural/page1.php
@@ -38,7 +38,7 @@ cylinders/stoves, which in turn can lead to gas leakage a nd thereby asphyxiatio
 <input type="radio" name="_2b" value="Y"> Yes
 <input type="radio" name="_2b" value="N"> No
 
-<p>b. Sewage lines are held rigidly between the house and the septic tank with no slack, that can result in breakage of sewage lines during an
+<p>c. Sewage lines are held rigidly between the house and the septic tank with no slack, that can result in breakage of sewage lines during an
 earthquake.</p>
 <input type="radio" name="_2c" value="Y"> Yes
 <input type="radio" name="_2c" value="N"> No
@@ -46,4 +46,3 @@ earthquake.</p>
 <br>
 <input type="submit" value="submit">
 </form>
-
---
+++ form7_burnt_clay_brick/3Economic_Structural/page3.php
@@ -0,0 +1,53 @@
+<!-- Burnt Clay Brick Masonry House -->
+<!-- Economic Loss-Inducing Factors related to the Structural Elements -->
+
+<form action="page1-backend.php">
+
+<!-- 4 -->
+<p>4. Structural Aspects</p>
+
+<p>4.1 Walls</p>
+<p>You may select 0 or more.</p>
+<input type="checkbox" name="_4-1[]" value="a"> Indirect or limited load paths
+<input type="checkbox" name="_4-1[]" value="b"> Large openings in walls
+<input type="checkbox" name="_4-1[]" value="c"> Walls unsymmetrical in one direction
+<input type="checkbox" name="_4-1[]" value="d"> Walls unsymmetrical in both directions
+
+<p>4.2 Roof design</p>
+<p>You may select 0 or more.</p>
+<input type="checkbox" name="_4-2[]" value="a"> Heavy roof
+<input type="checkbox" name="_4-2[]" value="b"> Pitched roof
+<input type="checkbox" name="_4-2[]" value="c"> Split roof
+<input type="checkbox" name="_4-2[]" value="d"> Weak diaphragm action tiled roof or separate planks
+<input type="checkbox" name="_4-2[]" value="e"> Large cut outs in diaphragm
+
+<p>4.3 Foundation-wall connection</p>
+<p>You may select 0 or more.</p>
+<input type="checkbox" name="_4-3[]" value="a"> No anchorage of wall reinforcement to foundation
+
+<p>4.4 Wall-wall connection</p>
+<p>You may select 0 or more.</p>
+<input type="checkbox" name="_4-4_1" value="a"> No roof band with pitched roof
+<input type="checkbox" name="_4-4_1" value="b"> No roof band with flat roof
+<input type="checkbox" name="_4-4_2[]" value="c"> No lintel band
+<input type="checkbox" name="_4-4_2[]" value="d"> No sill band
+<input type="checkbox" name="_4-4_2[]" value="e"> No plinth band
+<input type="checkbox" name="_4-4_2[]" value="f"> Arches/vaults without tie rods
+
+<p>4.5 Wall to roof/floor connection</p>
+<p>You may select 0 or more.</p>
+<input type="checkbox" name="_4-5[]" value="a">No/insufficient anchorage of vertical reinforcement from walls to roof/floor
+
+<p>4.6 Staircase</p>
+<input type="radio" name="_4-6" value="a"> Unsymmetrical location
+<input type="radio" name="_4-6" value="b"> Both top and bottom of staircase integrally built into the building frame
+<input type="radio" name="_4-6" value="c" checked> None of the Above
+
+<p>4.7 Large water tanks on roof</p>
+<input type="radio" name="_4-6" value="a"> Unsymmetrically located and integrally built staircase
+<input type="radio" name="_4-6" value="b"> Staircase not adequately separated from the house
+<input type="radio" name="_4-6" value="c" checked> None of the Above
+
+<br>
+<input type="submit" value="submit">
+</form>
