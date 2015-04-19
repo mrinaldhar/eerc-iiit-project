52656a1c7e23cfb429b8677f38618cc947174b4b
Smriti.Singh <smritis.31095@gmail.com>
2015-04-20 01:57:54 +0530

    Added score to parameters.

--- form1_stone/3Economic_Structural/page3.php
+++ form1_stone/3Economic_Structural/page3.php
@@ -6,50 +6,51 @@
 
     <p>4.1 Walls</p>
     <p>You may select 0 or more.</p>
-    <input type="checkbox" name="_4-1[]" value='a'/>Indirect or limited load paths<br/>
-    <input type="checkbox" name="_4-1[]" value='b'/>Large openings in walls<br/>
-    <input type="checkbox" name="_4-1[]" value='c'/>Walls unsymmetrical in one direction<br/>
+    <input type="checkbox" name="_4-1[]" value='a' id='-4'/>Indirect or limited load paths<br/>
+    <input type="checkbox" name="_4-1[]" value='b' id='-4'/>Large openings in walls<br/>
+    <input type="checkbox" name="_4-1[]" value='c' id='-3'/>Walls unsymmetrical in one direction<br/>
+    <input type="checkbox" name="_4-1[]" value='d' id='-4'/>Walls unsymmetrical in both directions<br/>
 
 
     <p>4.2 Roof design</p>
     <p>You may select 0 or more.</p>
-    <input type="checkbox" name="_4-2[]" value='a'/>Heavy roof<br/>
-    <input type="checkbox" name="_4-2[]" value='b'/>Pitched roof<br/>
-    <input type="checkbox" name="_4-2[]" value='c'/>Split roof<br/>
-    <input type="checkbox" name="_4-2[]" value='d'/>Weak diaphragm action tiled roof or separate planks<br/>
-    <input type="checkbox" name="_4-2[]" value='e'/>Large openings in roof<br/>
+    <input type="checkbox" name="_4-2[]" value='a' id='-4'/>Heavy roof<br/>
+    <input type="checkbox" name="_4-2[]" value='b' id='-4'/>Pitched roof<br/>
+    <input type="checkbox" name="_4-2[]" value='c' id='-4'/>Split roof<br/>
+    <input type="checkbox" name="_4-2[]" value='d' id='-4'/>Weak diaphragm action tiled roof or separate planks<br/>
+    <input type="checkbox" name="_4-2[]" value='e' id='-4'/>Large openings in roof<br/>
 
 
     <p>4.3 Foundation - wall connection</p>
     <p>You may select 0 or more.</p>
-    <input type="checkbox" name="_4-3[]" value='a'/>No anchorage of reinforcement from walls to foundation<br/>
+    <input type="checkbox" name="_4-3[]" value='a' id='-5'/>No anchorage of reinforcement from walls to foundation<br/>
 
 
     <p>4.4 Wall-wall connection</p>
     <p>You may select one out of the first two options, and 0 or more out of the rest.</p>
-    <input type="radio" name="_4-4" value='a'/>No roof band with pitched roof<br/>
-    <input type="radio" name="_4-4" value='b' checked/>No roof band with flat roof<br/>
-    <input type="checkbox" name="_4-4[]" value='c'/>No lintel band<br/>
-    <input type="checkbox" name="_4-4[]" value='d'/>No sill band<br/>
-    <input type="checkbox" name="_4-4[]" value='e'/>No plinth band<br/>
-    <input type="checkbox" name="_4-4[]" value='f'/>Arches/vaults without tie rods<br/>
+    <input type="radio" name="_4-4" value='a' id='-4'/>No roof band with pitched roof<br/>
+    <input type="radio" name="_4-4" value='b' id='0' checked/>No roof band with flat roof<br/>
+    <input type="checkbox" name="_4-4[]" value='c' id='-5'/>No lintel band<br/>
+    <input type="checkbox" name="_4-4[]" value='d' id='-2'/>No sill band<br/>
+    <input type="checkbox" name="_4-4[]" value='e' id='0'/>No plinth band<br/>
+    <input type="checkbox" name="_4-4[]" value='f' id='-5'/>Arches/vaults without tie rods<br/>
 
 
     <p>4.5 Wall to roof/floor connection</p>
     <p>You may select 0 or more.</p>
-    <input type="checkbox" name="_4-5[]" value='a'/>No/insufficient anchorage of vertical reinforcement from walls to roof/floor<br/>
+    <input type="checkbox" name="_4-5[]" value='a' id='-3'/>No/insufficient anchorage of vertical reinforcement from walls to roof/floor<br/>
 
 
     <p>4.6 Staircase</p>
     <p>You may select 0 or more.</p>
-    <input type="checkbox" name="_4-6[]" value='a'/>Unsymmetrical location<br/>
-    <input type="checkbox" name="_4-6[]" value='b'/>Both top and bottom integrally built into the building frame<br/>
+    <input type="checkbox" name="_4-6[]" value='a' id='-5'/>Unsymmetrical location<br/>
+    <input type="checkbox" name="_4-6[]" value='b' id='-5'/>Both top and bottom integrally built into the building frame<br/>
 
 
     <p>4.7 Large water tanks flat on roof</p>
-    <input type="radio" name="_4-7" value='a'/>Unsymmetrically located and integrally built staircase<br/>
-    <input type="radio" name="_4-7" value='b'/>Staircase not adequately separated from the house<br/>
-    <input type="radio" name="_4-7" value='c' checked/>None of these
+    <input type="radio" name="_4-7" value='a' id='-3'/>Unsymmetrically located and integrally built staircase<br/>
+    <input type="radio" name="_4-7" value='b' id='-3'/>Staircase not adequately separated from the house<br/>
+    <input type="radio" name="_4-7" value='c' id='0' checked/>None of these
 
 
     <br/>
--- form1_stone/4Economic_NonStructural/page1.php
+++ form1_stone/4Economic_NonStructural/page1.php
@@ -6,40 +6,27 @@
 
     <p>1.1 Shelves</p>
     <p>You may select 0 or more.</p>
-    <input type="checkbox" name="_1-1[]" value='a'/>Not anchored to structural system<br/>
-    <input type="checkbox" name="_1-1[]" value='b'/>Contents not strapped<br/>
+    <input type="checkbox" name="_1-1[]" value='a' id='-20'/>Not anchored to structural system<br/>
+    <input type="checkbox" name="_1-1[]" value='b' id='-10'/>Contents not strapped<br/>
 
     <p>1.2 Roof design</p>
     <p>You may select 0 or more.</p>
-    <input type="checkbox" name="_1-2[]" value='a'/>Items on lofts<br/>
+    <input type="checkbox" name="_1-2[]" value='a' id='-20'/>Items on lofts<br/>
 
     <p>1.3 Hangings from roof/floor and from walls</p>
     <p>You may select 0 or more.</p>
-    <input type="checkbox" name="_1-3[]" value='a'/>Heavy weight and unstrapped<br/>
+    <input type="checkbox" name="_1-3[]" value='a' id='-5'/>Heavy weight and unstrapped<br/>
 
     <p>1.4 Gas cylinders and geysers</p>
     <p>You may select 0 or more.</p>
-    <input type="checkbox" name="_1-4[]" value='a'/>Unsecured to the wall<br/>
+    <input type="checkbox" name="_1-4[]" value='a' id='-5'/>Unsecured to the wall<br/>
 
     <p>1.5 Items on adjacent building</p>
     <p>You may select 0 or more.</p>
-    <input type="checkbox" name="_1-5[]" value='a'/>Unsecured to adjacent building<br/>
+    <input type="checkbox" name="_1-5[]" value='a' id='-20'/>Unsecured to adjacent building<br/>
     <br/>
 
-    <p>2. Lifelines</p>
-
-    <p>2.1 Electric supply</p>
-    <p>You may select 0 or more.</p>
-    <input type="checkbox" name="_2-1[]" value='a'/>Cable is taught between house and pole<br/>
-
-    <p>2.2 Water supply</p>
-    <p>You may select 0 or more.</p>
-    <input type="checkbox" name="_2-2[]" value='a'/>Pipeline is rigidly held between house and main line<br/>
-
-    <p>2.3 Sewage</p>
-    <p>You may select 0 or more.</p>
-    <input type="checkbox" name="_2-3[]" value='a'/>Sewage line is rigidly held between house and main line<br/>
-    <br/>
+    
 
     <input type="submit" value="Submit"/>
 </form>
